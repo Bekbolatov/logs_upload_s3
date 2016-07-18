3739a06e75375972a1bc03acc397bcfa9eb08b31
Renat Bekbolatov <rbekbolatov@avvo.com>
2016-07-17 17:45:09 -0700

    more refactor - more efficient structure

--- common/bin/task_definition/publish_new_revision.sh
+++ common/bin/task_definition/publish_new_revision.sh
@@ -3,19 +3,21 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 source $DIR/../prepare_deployment.sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ $SKIP_DOCKER_PUSH != "true" ]]; then
     source $DIR/../docker/build_push.sh
 else
     echo "WARNING: Skipping Docker build/push"
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ $SKIP_TASK_REGISTRATION != "true" ]]; then
     source $DIR/../aws/register_task_definition.sh
 else
     echo "WARNING: Skipping task registration"
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../prepare_deployment_with_revision.sh
