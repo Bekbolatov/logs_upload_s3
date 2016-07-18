d4a2025058eed0999c655c50bf7d286d38e29423
Renat Bekbolatov <rbekbolatov@avvo.com>
2016-07-18 10:26:54 -0700

    parametrize distribution location

--- conf_manage/redeploy.sh
+++ conf_manage/redeploy.sh
@@ -14,7 +14,7 @@ cp -rf ../logs_upload/conf/* $TARGET_CONF_DIR/.
 cp -rf ../sparkydots/conf/* $TARGET_CONF_DIR/.
 ############################################################
 
-source ../common/bin/task_definition/publish_new_revision.sh
+source $DISTRIB_HOME/bin/task_definition/publish_new_revision.sh
 
-source ../common/bin/aws/run_task.sh
+source $DISTRIB_HOME/bin/aws/run_task.sh
 
--- logs_upload/redeploy.sh
+++ logs_upload/redeploy.sh
@@ -9,6 +9,6 @@ set +o allexport
 ############################################################
 ############################################################
 
-source ../common/bin/task_definition/publish_new_revision.sh
+source $DISTRIB_HOME/bin/task_definition/publish_new_revision.sh
 
-source ../common/bin/aws/update_service.sh
+source $DISTRIB_HOME/bin/aws/update_service.sh
