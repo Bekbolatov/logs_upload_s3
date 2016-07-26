79e740579f3189a12f6b2040028b972b0f012589
Renat Bekbolatov <rbekbolatov@avvo.com>
2016-07-25 17:28:34 -0700

    authorize ip add 8999 and modify resource reqs for polling tasks

--- common/bin/aws/security_group/authorize_my_ip.sh
+++ common/bin/aws/security_group/authorize_my_ip.sh
@@ -7,25 +7,33 @@ SG_GROUP_RDS="sg-03b8cd65"
 
 
 CIDR=$(aws ec2 describe-security-groups --group-id $SG_GROUP_ECS  | jq -r '.SecurityGroups[0].IpPermissions | map(select(.FromPort == 22)) | .[0].IpRanges[0].CidrIp')
-echo "Previous CIDR for ECS: $CIDR"
+NEWCIDR=$(curl ifconfig.co)"/32"
+
+if [[ "$CIDR" == "$NEWCIDR" ]]; then
+    echo "CIDR has not changed: $CIDR"
+    exit 0
+fi
+
+echo "Updating permission: $CIDR -> $NEWCIDR"
+
 if [[ "$CIDR" != "null" ]]; then
     aws ec2 revoke-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 22 --cidr $CIDR
     aws ec2 revoke-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 9002 --cidr $CIDR
+    aws ec2 revoke-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 8999 --cidr $CIDR
 fi
 
 # No need to connect to RDS for now
 #CIDR=$(aws ec2 describe-security-groups --group-id $SG_GROUP_RDS  | jq -r '.SecurityGroups[0].IpPermissions | map(select(.FromPort == 22)) | .[0].IpRanges[0].CidrIp')
 #echo "Previous CIDR for RDS: $CIDR"
-#if [[ "$CIDR" != "null" ]]; then
-#    aws ec2 revoke-security-group-ingress --group-id $SG_GROUP_RDS --protocol tcp --port 3306 --cidr $CIDR
+#if [[ "NEWCIDR" != "null" ]]; then
+#    aws ec2 revoke-security-group-ingress --group-id $SG_GROUP_RDS --protocol tcp --port 3306 --cidr NEWCIDR
 #fi
 
 
-CIDR=$(curl ifconfig.co)"/32"
-echo "New CIDR: $CIDR"
 if [[ "$CIDR" != "null" ]]; then
     sleep 5
-    aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 22 --cidr $CIDR
-    aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 9002 --cidr $CIDR
-    #aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_RDS --protocol tcp --port 3306 --cidr $CIDR
+    aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 22 --cidr $NEWCIDR
+    aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 9002 --cidr $NEWCIDR
+    aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_ECS --protocol tcp --port 8999 --cidr $NEWCIDR
+    #aws ec2 authorize-security-group-ingress --group-id $SG_GROUP_RDS --protocol tcp --port 3306 --cidr NEWCIDR
 fi
